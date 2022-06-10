3163948dc8fa18fe2e9e312d635f24eca1b505a1
thedonmon <22967202+thedonmon@users.noreply.github.com>
2022-06-10 17:52:08 -0400

    update scripts

---
+++ src/samplescripts.sh
@@ -0,0 +1,19 @@
+npx ts-node src/index.ts airdrop-nft \
+-m /Users/don/Downloads/openera/openera/karmandimints.json \
+-al /Users/don/SourceCode/spl-airdrop/mintransferk.json \
+-k ~/.config/solana/openera.json \ 
+-s true \
+-b 50 \
+-r https://rpcurl/
+
+npx ts-node src/index.ts airdrop-nft \
+-m /Users/don/Downloads/openera/openera/laurentismints.json \
+-al /Users/don/SourceCode/spl-airdrop/mintransferl.json \
+-k ~/.config/solana/openera.json -s true -b 50 \
+-r https://rpcurl/
+
+npx ts-node src/index.ts airdrop-nft \
+-m /Users/don/Downloads/openera/openera/persidephonMints.json \
+-al /Users/don/SourceCode/spl-airdrop/mintransferp.json \
+-k ~/.config/solana/openera.json -s true -b 50 \
+-r https://rpcurl/
